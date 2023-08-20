0197282187bb170e0ca2f3c45932d6f3416e1902
Christian Hedel <cwasilei@gmail.com>
2023-08-20 13:51:17 +0200

    offline support

--- main.py
+++ main.py
@@ -12,41 +12,44 @@ H = 480
 
 if __name__ == "__main__":
   if len(sys.argv) < 2:
-    print(f"\n*** Usage: {sys.argv[0]} <ps5-IP>")
-    exit(-1)
-
-  ip_address = sys.argv[1]
+    print(f"\n*** Usage with PS5: {sys.argv[0]} <PS5-IP>")
+    ip_address = None
+  else:
+    ip_address = sys.argv[1]
 
   pygame.init()
-  screen = pygame.display.set_mode((W,H), pygame.RESIZABLE)
+  screen = pygame.display.set_mode((W,H), pygame.FULLSCREEN)
   monitor_size = [pygame.display.Info().current_w, pygame.display.Info().current_h]
 
   sprites = pygame.sprite.Group()
   sprites.add(Speedometer(240, 130, (W-240)//2, (H-130)//2))
-  sprites.add(GearIndicator(60,60, 740, 400))
+  sprites.add(GearIndicator(60,60, 720, 400))
 
-  data = Mock()
-  data.car_speed = 0
-  data.current_gear = 0
+  packet = Mock()
+  packet.car_speed = 23
+  packet.current_gear = 4
 
-  sprites.update(data)
+  sprites.update(packet)
   sprites.draw(screen)
   pygame.display.update()
 
-  fullscreen = False
+  fullscreen = True
 
-  listener = Listener(ip_address)
-  listener.start()
+  if ip_address != None:
+    listener = Listener(ip_address)
+    listener.start()
 
   while True:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
-        listener.close()
+        if ip_address != None:
+          listener.close()
         pygame.quit()
         sys.exit()
       if event.type == KEYDOWN:
         if event.key == K_ESCAPE:
-          listener.close()
+          if ip_address != None:
+            listener.close()
           pygame.quit()
           sys.exit()
         if event.key == K_f:
@@ -56,7 +59,8 @@ if __name__ == "__main__":
           else:
             screen = pygame.display.set_mode((W,H), pygame.RESIZABLE)
 
-    packet = listener.get()
+    if ip_address != None:
+      packet = listener.get()
     sprites.update(packet)
     sprites.draw(screen)
     pygame.display.update()
