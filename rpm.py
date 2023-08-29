f54c09ee30ebab3e5bb3e62991f55b2c90539056
Christian Hedel <cwasilei@gmail.com>
2023-08-29 14:43:02 +0200

    ux

--- rpm.py
+++ rpm.py
@@ -4,10 +4,7 @@ class RPM(pygame.sprite.Sprite):
   def __init__(self, x, y, w, h, name, color=(0,0,0)):
     super().__init__()
     self.name = name
-    if self.name % 10 == 0:
-      self.image = pygame.Surface((w,h+35))
-    else:
-      self.image = pygame.Surface((w,h))
+    self.image = pygame.Surface((w,h))
     self.image.fill(color)
     self.rect = self.image.get_rect()
     self.rect.x = x
