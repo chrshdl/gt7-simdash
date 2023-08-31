8f4833bcb6a3d8c27e5af1be40439c0247eb8613
Christian Hedel <cwasilei@gmail.com>
2023-08-31 17:07:52 +0200

    refactor

--- main.py
+++ main.py
@@ -9,6 +9,29 @@ from gear import GearIndicator
 from rpm import RPM
 from unittest.mock import Mock
 
+def create_dash(W, rpm_alert_min, rpm_alert_max):
+  sprites = pygame.sprite.Group()
+  sprites.add(Speedometer(360, 160, 0, 200))
+  sprites.add(GearIndicator(260,260, 520, 120))
+
+  rpm_min = int(rpm_alert_min) // 100
+  rpm_max = int(rpm_alert_max) // 100
+
+  margin = 1
+  offset = 15
+  width = (W - (rpm_max * margin) - offset) // rpm_max
+  height = 35
+
+  sprites.add(
+    RPM(
+      offset + (margin + width) * step + margin,
+      50,
+      width,
+      height,
+      step) for step in range(rpm_max + 1)
+  )
+  return sprites
+
 
 def run(conf):
 
@@ -21,48 +44,25 @@ def run(conf):
     from granturismo.intake import Feed
     listener = Feed(ip_address)
     listener.start()
+    packet = listener.get()
   else:
     packet = Mock()
     packet.car_speed = 0/3.6
     packet.current_gear = 3
     packet.engine_rpm = 0.0
-    packet.rpm_alert.min = 4500
+    packet.rpm_alert.min = 7000
+    packet.rpm_alert.max = 9000
 
   pygame.init()
+  clock = pygame.time.Clock()
+  monitor_size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
 
   if fullscreen:
-    screen = pygame.display.set_mode((W,H), pygame.FULLSCREEN)
+    screen = pygame.display.set_mode(monitor_size, pygame.FULLSCREEN)
   else:
     screen = pygame.display.set_mode((W,H), pygame.RESIZABLE)
 
-  monitor_size = [pygame.display.Info().current_w, pygame.display.Info().current_h]
-  clock = pygame.time.Clock()
-
-  sprites = pygame.sprite.Group()
-
-  sprites.add(Speedometer(360, 160, 0, 200))
-  sprites.add(GearIndicator(260,260, 520, 120))
-
-  packet = listener.get()
-  rpm_max = int(packet.rpm_alert.max) // 100
-  rpm_min = int(packet.rpm_alert.min) // 100
-  print(f"rpm min={rpm_min}")
-  print(f"rpm max={rpm_max}")
-
-  width = W//(rpm_max + 10)
-  height = 40
-  margin = 1
-  offset = 2
-
-  sprites.add(
-    RPM(
-      offset + (margin + width) * step + margin,
-      20,
-      width,
-      height,
-      step) for step in range(rpm_max + 10)
-  )
-
+  dash = create_dash(W, packet.rpm_alert.min, packet.rpm_alert.max)
 
   while True:
     for event in pygame.event.get():
@@ -87,11 +87,11 @@ def run(conf):
     if ip_address != None:
       packet = listener.get()
     else:
-      packet.engine_rpm = (packet.engine_rpm + 30) % 7001
+      packet.engine_rpm = (packet.engine_rpm + 50) % packet.rpm_alert.max
       packet.car_speed = (packet.car_speed + 1) % 255
 
-    sprites.update(packet)
-    sprites.draw(screen)
+    dash.update(packet)
+    dash.draw(screen)
     pygame.display.flip()
     clock.tick(60)
 
--- rpm.py
+++ rpm.py
@@ -1,11 +1,21 @@
 import pygame
 
+GREEN = (40,90,27)
+BLACK = (0,0,0)
+RED = (210,62,54)
+DARK_RED = (82,24,21)
+GREY = (20,20,20)
+DARK_GREY = (10,10,10)
+BLUE = (0,0,255)
+
 class RPM(pygame.sprite.Sprite):
-  def __init__(self, x, y, w, h, name, color=(0,0,0)):
+  def __init__(self, x, y, w, h, name, color=BLACK):
     super().__init__()
     self.name = name
-    self.image = pygame.Surface((w,h))
-    self.image.fill(color)
+    if self.name % 10 == 0:
+      self.image = pygame.Surface((w,h+5))
+    else:
+      self.image = pygame.Surface((w,h))
     self.rect = self.image.get_rect()
     self.rect.x = x
     self.rect.y = y
@@ -16,14 +26,14 @@ class RPM(pygame.sprite.Sprite):
 
     if self.name <= rpm:
       if rpm >= rpm_alert_min and self.name >= rpm_alert_min:
-        self.image.fill((82,24,21))
+        self.image.fill(RED)
       else:
-        self.image.fill((5,44,27))
+        self.image.fill(GREEN)
     else:
-      if self.name % 10 == 0 and self.name >= rpm_alert_min:
-        self.image.fill((82,24,21))
+      #if self.name % 10 == 0 and self.name >= rpm_alert_min:
+      if self.name >= rpm_alert_min:
+        self.image.fill(DARK_RED)
       elif self.name % 10 == 0 and not self.name >= rpm_alert_min:
-        self.image.fill((30,30,30))
+        self.image.fill(GREY)
       else:
-        self.image.fill((0,0,0))
-
+        self.image.fill(DARK_GREY)
