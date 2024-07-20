6286546e4dc4174a54783ea218f75b730d86a7f8
Christian Hedel <cwasilei@gmail.com>
2024-07-20 15:57:59 +0200

    new rpm design

--- hmi.py
+++ hmi.py
@@ -20,12 +20,15 @@ class HMI:
 
         self._car_id = car_id
 
-        rpm_max = self._normalize(rpm_max)
         self.rpm = pygame.sprite.Group()
         self.add_rpm(rpm_min, rpm_max)
 
     def add_rpm(self, rpm_min, rpm_max):
         y = 158
+
+        rpm_min = int(rpm_min - rpm_max * 0.04) // 100
+        rpm_max = self._normalize(rpm_max)
+
         screen_width = self.screen.get_size()[0]
         self.rpm.add(
             RPM(screen_width, y, rpm_min, rpm_max, step) for step in range(rpm_max + 1)
@@ -36,7 +39,7 @@ class HMI:
 
     def refresh_rpm(self, rpm_min, rpm_max):
         self.remove_all_rpm()
-        self.add_rpm(rpm_min, self._normalize(rpm_max))
+        self.add_rpm(rpm_min, rpm_max)
 
     def remove_all_rpm(self):
         for item in self.rpm:
--- rpm.py
+++ rpm.py
@@ -3,38 +3,37 @@ from color import Color
 
 
 class RPM(pygame.sprite.Sprite):
-    def __init__(self, screen_width, y, rpm_alert_min, rpm_alert_max, step):
+    def __init__(self, screen_width, y, rpm_min, rpm_max, step):
         super().__init__()
-        self.rpm_alert_min = rpm_alert_min
-        self.rpm_alert_max = rpm_alert_max
-        self.rpm_alert_min_thres = (
-            int(self.rpm_alert_min - self.rpm_alert_max * 0.04) // 100
-        )
+        self.rpm_min = rpm_min
+        self.rpm_max = rpm_max
         margin = 1
-        w = 3
-        h = 35
-        offset_center = (screen_width - rpm_alert_max * (margin + w)) // 2
-        pos = ((offset_center + (margin + w) * step + margin), y)
+        self.w = 3
+        self.h = 35
+        offset_center = (screen_width - rpm_max * (margin + self.w)) // 2
+        pos = ((offset_center + (margin + self.w) * step + margin), y)
         self.step = step
         if self.step % 10 == 0:
-            self.image = pygame.Surface((w, h + 7)).convert()
+            self.image = pygame.Surface((self.w, self.h + 7)).convert()
+            if self.step < rpm_min:
+                self.image.fill(Color.BLUE.rgb())
+            else:
+                self.image.fill(Color.RED.rgb())
         else:
-            self.image = pygame.Surface((w, h)).convert()
+            self.image = pygame.Surface((self.w, self.h)).convert()
+
         self.rect = self.image.get_rect(topleft=pos)
 
     def update(self, current_rpm):
         if self.step <= current_rpm:
-            if (
-                current_rpm >= self.rpm_alert_min_thres
-                and self.step >= self.rpm_alert_min_thres
-            ):
+            if current_rpm >= self.rpm_min and self.step >= self.rpm_min:
                 self.image.fill(Color.RED.rgb())
             else:
                 self.image.fill(Color.BLUE.rgb())
         else:
-            if self.step >= self.rpm_alert_min_thres:
-                self.image.fill(Color.DARK_RED.rgb())
-            elif self.step % 10 == 0 and not self.step >= self.rpm_alert_min_thres:
-                self.image.fill(Color.GREY.rgb())
+            if self.step >= self.rpm_min:
+                self.image.fill(Color.DARK_RED.rgb(), (0, 0, self.w, self.h))
+            elif self.step % 10 == 0 and not self.step >= self.rpm_min:
+                self.image.fill(Color.GREY.rgb(), (0, 0, self.w, self.h))
             else:
-                self.image.fill(Color.DARK_GREY.rgb())
+                self.image.fill(Color.DARK_GREY.rgb(), (0, 0, self.w, self.h))
