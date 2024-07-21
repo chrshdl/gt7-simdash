d8c59be14e6ff609bc4e1931300464e60e802484
Christian Hedel <35096278+chrshdl@users.noreply.github.com>
2024-07-22 00:47:00 +0200

    no comment

--- hmi/gear.py
+++ hmi/gear.py
@@ -3,18 +3,9 @@ from hmi.widget import AbstractWidget
 
 
 class GearIndicator(AbstractWidget):
-    #    def __init__(self, w, h, x, y):
-    #        super().__init__()
-    #        self.image = pygame.Surface((w, h)).convert()
-    #        self.rect = self.image.get_rect(center=(x, y))
-    #        self.font = pygame.font.Font("fonts/digital-7-mono.ttf", 240)
-
-    # def draw_overlay(self):
-    #    pygame.draw.rect(self.image, Color.DARK_GREY.rgb(), self.image.get_rect(), 0, 4)
-    #    pygame.draw.rect(self.image, Color.GREY.rgb(), self.image.get_rect(), 1, 4)
-
     def update(self, data):
         super().update(data)
+        
         if not data.flags.in_gear:
             gear = "N"
         else:
