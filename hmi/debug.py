9ebaa4a694b934ab3fb489f5bc0d3dc60cae4731
Christian Hedel <35096278+chrshdl@users.noreply.github.com>
2024-07-25 22:19:22 +0200

    we are on track

--- hmi/debug.py
+++ hmi/debug.py
@@ -14,16 +14,19 @@ class DebugScreen(Widget):
     def update(self, dt, packet):
         super().update()
 
+        self.car_on_track = packet.flags.car_on_track
         dbg_track = f"on track: {self.car_on_track}"
         dbg = self.header_font.render(dbg_track, False, Color.LIGHT_GREY.rgb())
         midtop = tuple(map(sum, zip(self.image.get_rect().midtop, (0, 10))))
         self.image.blit(dbg, dbg.get_rect(midtop=midtop))
 
+        self.loading_or_processing = packet.flags.loading_or_processing
         dbg_loading = f"loading: {self.loading_or_processing}"
         dbg2 = self.header_font.render(dbg_loading, False, Color.LIGHT_GREY.rgb())
         center = self.image.get_rect().center
         self.image.blit(dbg2, dbg2.get_rect(center=center))
 
+        self.paused = packet.flags.paused
         dbg_paused = f"paused: {self.paused}"
         dbg3 = self.header_font.render(dbg_paused, False, Color.LIGHT_GREY.rgb())
         midbottom = tuple(map(sum, zip(self.image.get_rect().midbottom, (0, -10))))
