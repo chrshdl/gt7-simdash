62967db8a4db48e43d990f48d782b92fdcf8547c
Christian Hedel <35096278+chrshdl@users.noreply.github.com>
2024-07-26 01:29:30 +0200

    better naming

--- hmi/event.py
+++ hmi/event.py
@@ -5,7 +5,7 @@ from enum import Enum
 class Event(Enum):
     NEW_CAR_EVENT = (1, pygame.USEREVENT + 1, "NEW_CAR_EVENT")
     HMI_STARTED_EVENT = (2, pygame.USEREVENT + 2, "HMI_STARTED_EVENT")
-    LEDS_SHOW_ALL_RED = (3, pygame.USEREVENT + 3, "LEDS_SHOW_ALL_RED")
+    LEDS_SHOW = (3, pygame.USEREVENT + 3, "LEDS_SHOW")
     LEDS_CLEAR_ALL = (4, pygame.USEREVENT + 4, "LEDS_CLEAR_ALL")
 
     def type(self):
