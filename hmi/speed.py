3241ed6a846d99c6e9ffe00356718c171956f97d
Christian Hedel <35096278+chrshdl@users.noreply.github.com>
2024-07-26 01:38:59 +0200

    remove led logic

--- hmi/speed.py
+++ hmi/speed.py
@@ -1,13 +1,12 @@
 from hmi.settings import *
 from hmi.color import Color
 from hmi.widget import Widget
-from hmi.event import Event
 
 
 class Speedometer(Widget):
     def __init__(self, groups, w, h, main_fsize=108, header_fsize=46):
         super().__init__(groups, w, h, main_fsize, header_fsize)
-        self.rect.topleft = POS["speed"]
+        self.rect.center = POS["speed"]
         self.car_speed = 0
         self.led_active = False
 
@@ -18,22 +17,6 @@ class Speedometer(Widget):
             self.car_speed = packet.car_speed * 3.6
         else:
             self.car_speed = (self.car_speed + (dt * 25)) % 400
-
-        if self.led_active:
-            if self.car_speed < 60:
-                self.led_active = False
-                pygame.event.post(pygame.event.Event(Event.LEDS_CLEAR_ALL.type()))
-        else:
-            if self.car_speed > 65 and self.car_speed < 75:
-                self.led_active = True
-                pygame.event.post(
-                    pygame.event.Event(Event.LEDS_SHOW_ALL_RED.type(), message=2)
-                )
-            elif self.car_speed > 76 and self.car_speed < 90:
-                self.led_active = True
-                pygame.event.post(
-                    pygame.event.Event(Event.LEDS_SHOW_ALL_RED.type(), message=4)
-                )
 
         speed = str(int(self.car_speed))
         speed = speed.center(len(speed))
