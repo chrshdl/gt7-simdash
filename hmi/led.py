edf25850a1b2624048f417adbcd3a1bb39e5bb64
Christian Hedel <35096278+chrshdl@users.noreply.github.com>
2024-07-26 16:39:14 +0200

    :art:

--- hmi/led.py
+++ hmi/led.py
@@ -2,18 +2,18 @@ from hmi.color import Color
 from hmi.event import Event
 import platform
 import logging
-import time
 
 from logformatter import LogFormatter
 
-if platform.machine() == "aarch64":
+RASPBERRY_PI = "aarch64"
+if platform.machine() == RASPBERRY_PI:
     import blinkt
 
 
 class LED:
     def __init__(self):
 
-        if platform.machine() == "aarch64":
+        if platform.machine() == RASPBERRY_PI:
             blinkt.set_brightness(0.05)
 
         self.logger = logging.getLogger(self.__class__.__name__)
@@ -28,40 +28,31 @@ class LED:
         self.colors.append(Color.BLUE.rgb())
         self.colors.append(Color.GREEN.rgb())
         self.colors.append(Color.GREEN.rgb())
-        self.leds_to_set = 0
 
-    def update(self, events):
+    def draw(self, events):
         for e in events:
             if e.type == Event.LEDS_SHOW.type():
-                if platform.machine() == "aarch64":
+                if platform.machine() == RASPBERRY_PI:
                     self.show(e.message)
 
             if e.type == Event.LEDS_CLEAR_ALL.type():
-                if platform.machine() == "aarch64":
-                    self.leds_to_set = 0
+                if platform.machine() == RASPBERRY_PI:
                     blinkt.clear()
                     blinkt.show()
 
     def clear_all(self):
-        self.logger.info("LEDS CLEARED")
+        # self.logger.info("LEDS CLEARED")
         blinkt.clear()
         blinkt.show()
 
     def show(self, target):
-        self.logger.info(f"LEDS ARE ACTIVE: {target}")
-        if target == 10:
-            if self.leds_to_set != target:
-                self.leds_to_set = target
-                blinkt.set_all(
-                        self.colors[3][0],
-                        self.colors[3][1],
-                        self.colors[3][2]
-                )
-                blinkt.show()
-        else:
-            for i in range(target // 2):
-                blinkt.set_pixel(i, self.colors[i][0], self.colors[i][1], self.colors[i][2])
-                blinkt.set_pixel(
-                    blinkt.NUM_PIXELS - 1 - i, self.colors[i][0], self.colors[i][1], self.colors[i][2]
-                )
-            blinkt.show()
+        # self.logger.info(f"LEDS ARE ACTIVE: {target}")
+        for i in range(target // 2):
+            blinkt.set_pixel(i, self.colors[i][0], self.colors[i][1], self.colors[i][2])
+            blinkt.set_pixel(
+                blinkt.NUM_PIXELS - 1 - i,
+                self.colors[i][0],
+                self.colors[i][1],
+                self.colors[i][2],
+            )
+        blinkt.show()
