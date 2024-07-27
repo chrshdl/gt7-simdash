822be508b14d868afd30225fa68f2e6cd6c8a8ad
Christian Hedel <35096278+chrshdl@users.noreply.github.com>
2024-07-28 00:15:20 +0200

    current lap time calculation

--- hmi/lap.py
+++ hmi/lap.py
@@ -1,37 +1,50 @@
 import time
+from datetime import datetime
 from hmi.settings import *
 from hmi.color import Color
 from hmi.widget import Widget
 
 
 class CurrentLap(Widget):
-    def __init__(self, groups, w, h):
-        super().__init__(groups, w, h)
-        self.rect.center = POS["curr_lap"]
+    def __init__(self, groups, w, h, main_fsize=40, header_fsize=42):
+        super().__init__(groups, w, h, main_fsize, header_fsize)
+        self.rect.center = POS["curr_lap_time"]
+        self.delta_time = 0
+        self.previous_lap = 0
+        self.previous_lap_time = 0
 
     def update(self, dt, packet=None):
         super().update()
 
-        llt = packet.last_lap_time
+        lap_time_str = "--:--"
 
-        if llt is None:
-            llt_str = "--:--"
+        if packet.lap_count == 0 or packet.lap_count == None:
+            self.delta_time = packet.received_time
+        if packet.lap_count != 0 and packet.lap_count == self.previous_lap + 1:
+            self.delta_time = packet.received_time
+            self.previous_lap = packet.lap_count
+            previous_time = datetime.strftime(datetime.utcfromtimestamp(self.previous_lap_time), "%M:%S.%f")[:-3]
+            print(f"previous lap time: {previous_time}")
+        if packet.laps_in_race != None and packet.lap_count != None and packet.laps_in_race < packet.lap_count:
+            lap_time = self.previous_lap_time
+            self.previous_lap = 0
         else:
-            llt_str = time.strftime(
-                "%M:%S.{}".format(llt % 1000), time.gmtime(llt / 1000.0)
-            )
-        llt_render = self.main_font.render(llt_str, False, Color.GREEN.rgb())
+            lap_time = packet.received_time - self.delta_time
+            self.previous_lap_time = lap_time
+        lap_time_str = datetime.strftime(datetime.utcfromtimestamp(lap_time), "%M:%S.%f")[:-3]
+
+        llt_render = self.main_font.render(lap_time_str, False, Color.GREEN.rgb())
         res = tuple(map(sum, zip(self.image.get_rect().midbottom, (0, 0))))
         self.image.blit(llt_render, llt_render.get_rect(midbottom=res))
-        label = self.header_font.render("Last Lap", False, Color.WHITE.rgb())
-        midtop = tuple(map(sum, zip(self.image.get_rect().midtop, (0, 10))))
+        label = self.header_font.render("Current Lap", False, Color.WHITE.rgb())
+        midtop = tuple(map(sum, zip(self.image.get_rect().midtop, (2, 10))))
         self.image.blit(label, label.get_rect(midtop=midtop))
 
 
 class BestLap(Widget):
-    def __init__(self, groups, w, h):
-        super().__init__(groups, w, h)
-        self.rect.center = POS["best_lap"]
+    def __init__(self, groups, w, h, main_fsize=40, header_fsize=42):
+        super().__init__(groups, w, h, main_fsize, header_fsize)
+        self.rect.center = POS["best_lap_time"]
 
     def update(self, dt, packet=None):
         super().update()
@@ -42,7 +55,7 @@ class BestLap(Widget):
             blt_str = "--:--"
         else:
             blt_str = time.strftime(
-                "%M:%S.{}".format(blt % 1000), time.gmtime(blt / 1000.0)
+                    "%M:%S.{}".format(blt % 1000), time.gmtime(blt / 1000.0)
             )
         blt_render = self.main_font.render(blt_str, False, Color.GREEN.rgb())
         res = tuple(map(sum, zip(self.image.get_rect().midbottom, (0, 0))))
@@ -52,23 +65,22 @@ class BestLap(Widget):
         self.image.blit(label, label.get_rect(midtop=midtop))
 
 
-class Lap(Widget):
-    def __init__(self, groups, w, h, main_fsize=48):
-        super().__init__(groups, w, h, main_fsize)
-        self.rect.center = POS["lap"]
+class Laps(Widget):
+    def __init__(self, groups, w, h, main_fsize=48, header_fsize=42):
+        super().__init__(groups, w, h, main_fsize, header_fsize)
+        self.rect.center = POS["laps"]
 
     def update(self, dt, packet=None):
         super().update()
 
-        curr_lap = packet.lap_count
-        total_laps = packet.laps_in_race
+        current = packet.lap_count
+        total = packet.laps_in_race
 
-        if curr_lap is None and total_laps is None:
-            curr_lap = 0
-            total_laps = 0
+        current = 0 if current is None else current
+        total = 0 if total is None else total
 
         lap_render = self.main_font.render(
-            f"{min(curr_lap, total_laps)} / {total_laps}", False, Color.GREEN.rgb()
+            f"{min(current, total)} / {total}", False, Color.GREEN.rgb()
         )
         res = tuple(map(sum, zip(self.image.get_rect().midbottom, (0, 0))))
         self.image.blit(lap_render, lap_render.get_rect(midbottom=res))
