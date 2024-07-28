eeb00f239952aeb70ae96385edf2e139f0fe289e
Christian Hedel <35096278+chrshdl@users.noreply.github.com>
2024-07-28 08:28:11 +0200

    remove logger

--- hmi/led.py
+++ hmi/led.py
@@ -1,9 +1,6 @@
 from hmi.color import Color
 from hmi.event import Event
 import platform
-import logging
-
-from logformatter import LogFormatter
 
 RASPBERRY_PI = "aarch64"
 if platform.machine() == RASPBERRY_PI:
@@ -16,13 +13,6 @@ class LED:
         if platform.machine() == RASPBERRY_PI:
             blinkt.set_brightness(0.05)
 
-        self.logger = logging.getLogger(self.__class__.__name__)
-        self.logger.setLevel(logging.DEBUG)
-
-        sh = logging.StreamHandler()
-        sh.setLevel(logging.DEBUG)
-        sh.setFormatter(LogFormatter())
-        self.logger.addHandler(sh)
         self.colors = list()
         self.colors.append(Color.BLUE.rgb())
         self.colors.append(Color.BLUE.rgb())
@@ -39,12 +29,10 @@ class LED:
                         self.show(e.message)
 
     def clear_all(self):
-        # self.logger.info("LEDS CLEARED")
         blinkt.clear()
         blinkt.show()
 
     def show(self, target):
-        # self.logger.info(f"LEDS ARE ACTIVE: {target}")
         for i in range(target // 2):
             blinkt.set_pixel(i, self.colors[i][0], self.colors[i][1], self.colors[i][2])
             blinkt.set_pixel(
