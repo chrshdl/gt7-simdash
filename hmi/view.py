4c08958d053970d8c3fd471dbfc7ca6819368c48
Christian Hedel <cwasilei@gmail.com>
2024-08-03 15:45:39 +0200

    we have a view

--- hmi/view.py
+++ hmi/view.py
@@ -1,32 +1,34 @@
 import pygame
-from hmi.speed import Speedometer
-from hmi.gear import GearIndicator
-from hmi.rpm import RPM
-from hmi.debug import DebugScreen
-from hmi.lap import Laps, EstimatedLap, BestLap
+
+from hmi.widgets.led import LED
+from hmi.widgets.rpm import RPM
+from hmi.widgets.gear import GearIndicator
+from hmi.widgets.speed import Speedometer
+from hmi.widgets.lap import Laps, BestLap, EstimatedLap
 from hmi.properties import Color
 
 
-class HMI:
+class View:
     def __init__(self):
         self.screen = pygame.display.get_surface()
         self.telemetry = pygame.sprite.Group()
 
-        Speedometer(self.telemetry, 180, 130)
-        GearIndicator(self.telemetry, 180, 220)
         RPM(self.telemetry, 300, 33)
+        GearIndicator(self.telemetry, 180, 220)
+        Speedometer(self.telemetry, 180, 130)
         Laps(self.telemetry, 180, 85)
         EstimatedLap(self.telemetry, 185, 85)
         BestLap(self.telemetry, 185, 85)
-        DebugScreen(self.telemetry, 180, 110)
+        LED()
 
-    def draw(self, packet):
+    def update(self, packet):
         self.screen.fill(Color.BLACK.rgb())
         self.telemetry.update(packet)
         self.telemetry.draw(self.screen)
 
-    def update_rpm_alerts(self, rpm_min, rpm_max):
+    def update_rpm_alerts(self, rpmin, rpmax):
         for sprite in self.telemetry.sprites():
             if isinstance(sprite, RPM):
-                sprite.alert_min(rpm_min)
-                sprite.alert_max(rpm_max)
+                sprite.alert_min(rpmin)
+                sprite.alert_max(rpmax)
+
