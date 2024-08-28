53babd0213756fc17a8d28ed5d6a76049fce7844
Omega1902 <27062486+Omega1902@users.noreply.github.com>
2024-08-28 21:29:08 +0200

    filter localhost better, try .local

--- hmi/views/wizard.py
+++ hmi/views/wizard.py
@@ -1,48 +1,16 @@
-import re
-import socket
-
 import pygame
 
-from common.logger import Logger
+from common.ipv4 import get_ip_prefill
 from hmi.widgets.button import Button
 from hmi.widgets.textfield import Textfield
-
-# Regex pattern for matching IPv4 addresses
-IPV4_PATTERN = r"([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})"
-
-
-def get_host_ip_address() -> str:
-    return socket.gethostbyname(socket.gethostname())
-
-
-def get_ip_prefill(host_ip: str) -> str:
-    """Returns the standard group of the given IP_v4-address, if it not localhost
-    Example:
-    get_ip_prefill("192.168.1.4") == "192.168.1."
-    get_ip_prefill("127.0.0.1") == ""
-    """
-    if host_ip == "127.0.0.1":
-        return ""
-    matches = re.findall(IPV4_PATTERN, host_ip)
-    if len(matches) == 0:
-        return ""
-    first = int(matches[0][0])
-    if first <= 127:
-        return matches[0][0] + "."
-    if first <= 191:
-        return matches[0][0] + "." + matches[0][1] + "."
-    return matches[0][0] + "." + matches[0][1] + "." + matches[0][2] + "."
 
 
 class Wizard:
     def __init__(self):
-        self.logger = Logger(self.__class__.__name__).get()
         self.screen = pygame.display.get_surface()
         self.wizard = pygame.sprite.Group()
 
-        prefill_ip = get_ip_prefill(get_host_ip_address())
-        self.logger.info(f"prefill ip: {prefill_ip}")
-        self.tf = Textfield(self.wizard, 360, 80, text=prefill_ip)
+        self.tf = Textfield(self.wizard, 360, 80, text=get_ip_prefill())
 
         labels = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ".", "<", "OK"]
         self.buttons = [
