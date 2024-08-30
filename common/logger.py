bfe8e9544338166d270310e7e76f5c32646b494e
Omega1902 <27062486+Omega1902@users.noreply.github.com>
2024-08-30 15:59:19 +0200

    introduce mypy and adding type hints

--- common/logger.py
+++ common/logger.py
@@ -3,7 +3,7 @@ import logging
 
 class Logger:
     def __init__(self, name):
-        self.logger = logging.getLogger(name)
+        self.logger: logging.Logger = logging.getLogger(name)
         self.logger.setLevel(logging.DEBUG)
 
         sh = logging.StreamHandler()
@@ -11,22 +11,24 @@ class Logger:
         sh.setFormatter(LogFormatter())
         self.logger.addHandler(sh)
 
-    def get(self):
+    def get(self) -> logging.Logger:
         return self.logger
 
 
 class LogFormatter(logging.Formatter):
-    grey = "\x1b[38;20m"
-    yellow = "\x1b[33;20m"
-    red = "\x1b[91;20m"
-    reset = "\x1b[0m"
-    format = "%(asctime)s  | %(levelname)s | %(message)s  (%(filename)s:%(lineno)d)"
+    grey: str = "\x1b[38;20m"
+    yellow: str = "\x1b[33;20m"
+    red: str = "\x1b[91;20m"
+    reset: str = "\x1b[0m"
+    format_text: str = (
+        "%(asctime)s  | %(levelname)s | %(message)s  (%(filename)s:%(lineno)d)"
+    )
 
-    FORMATS = {
-        logging.DEBUG: grey + format + reset,
-        logging.INFO: grey + format + reset,
-        logging.WARNING: yellow + format + reset,
-        logging.ERROR: red + format + reset,
+    FORMATS: dict[int, str] = {
+        logging.DEBUG: grey + format_text + reset,
+        logging.INFO: grey + format_text + reset,
+        logging.WARNING: yellow + format_text + reset,
+        logging.ERROR: red + format_text + reset,
     }
 
     def format(self, record):
--- hmi/widgets/__init__.py
+++ hmi/widgets/__init__.py
@@ -1,3 +1,3 @@
-from common.evendispatcher import EventDispatcher
+from common.eventdispatcher import EventDispatcher
 from common.logger import Logger
 from hmi.widgets.widget import Widget
